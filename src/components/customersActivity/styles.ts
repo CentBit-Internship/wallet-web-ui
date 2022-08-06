70b6c1bfa005f24ca80249383ac64f1051c555c9
Akorede Olawole <akoredeolawole@gmail.com>
2022-08-06 08:11:15 +0100

    bug-fixes

--- src/components/customersActivity/styles.ts
+++ src/components/customersActivity/styles.ts
@@ -3,9 +3,10 @@ import styled from "styled-components";
 
 export const CustomerActivityBackground = styled.div`
     background: #232323;
-    height:280px;
+    height:320px;
     width: 270px;
     border-radius: 10px;
+    margin-top: 10px;
     box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
 `
 
@@ -13,6 +14,7 @@ export const CustomerActivityHeader = styled.div`
     color: white;
     font-size: 18px;
     font-weight: 700;
+    padding-top: 20px;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -33,4 +35,8 @@ export const CustomerActivityHeaderSubtitle = styled.div`
 export const CustomerActivityDot = styled.div`
     margin-top: -18px;
     font-size: 20px;
+`
+
+export const CustomerActivityContent = styled.div`
+    margin-top: 30px;
 `
